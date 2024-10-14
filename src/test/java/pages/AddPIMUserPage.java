304c31e916132e9f2852d95619d761592a39a88d
Muhammad Qaiser <muhammadqaiser64@yahoo.com>
2024-10-14 22:06:30 +0100

    sorted the PIM user aaded page

--- src/test/java/pages/AddPIMUserPage.java
+++ src/test/java/pages/AddPIMUserPage.java
@@ -23,7 +23,7 @@ public class AddPIMUserPage
     protected By password_con_field = By.xpath("//body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[2]/input[1]");
     protected By cancel_btn = By.xpath("//button[normalize-space()='Cancel']");
     protected By save_btn = By.xpath("//button[normalize-space()='Save']");
-
+    protected By success_msg = By.xpath("//p[contains(.,'Successfully Saved')]");
 
     public void addNewPIMUser(String Fname,String Mname, String Lname, String id, String Uname,String pass, String conPass)
     {
@@ -31,14 +31,13 @@ public class AddPIMUserPage
         Utility.waitForElement(driver, emp_first_name).sendKeys(Fname);
         Utility.waitForElement(driver, emp_middle_name).sendKeys(Mname);
         Utility.waitForElement(driver, emp_last_name).sendKeys(Lname);
-        Utility.waitForElement(driver, emp_id_field).clear();
-        Utility.sleep(2);
         Utility.waitForElement(driver, emp_id_field).sendKeys(id);
         Utility.waitForElement(driver, create_login_btn).click();
         Utility.waitForElement(driver, user_name_btn).sendKeys(Uname);
         Utility.waitForElement(driver, password_field).sendKeys(pass);
         Utility.waitForElement(driver, password_con_field).sendKeys(conPass);
         Utility.waitForElement(driver, save_btn).click();
+        Utility.waitForElement(driver, success_msg).isDisplayed();
     }
 
     public boolean checkCancelBtnAvailable()
