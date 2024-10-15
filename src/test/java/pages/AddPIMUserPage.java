77514104f1cf384936710a94fdbbdfc2aa2ad062
Muhammad Qaiser <muhammadqaiser64@yahoo.com>
2024-10-15 15:36:37 +0100

    added teststeps in PIM user not working for now

--- src/test/java/pages/AddPIMUserPage.java
+++ src/test/java/pages/AddPIMUserPage.java
@@ -27,6 +27,20 @@ public class AddPIMUserPage
     protected By save_btn = By.xpath("//button[normalize-space()='Save']");
     protected By success_msg = By.xpath("//p[contains(.,'Successfully Saved')]");
 
+
+
+
+    protected By other_id_field = By.xpath("//body/div[@id='app']/div/div/div/div/div/div/div/div/form/div/div/div[2]/div[1]/div[2]/input[1]");
+    protected By driving_license = By.xpath("//body/div[@id='app']/div/div/div/div/div/div/div/div/form/div/div[2]/div[1]/div[1]/div[2]/input[1]");
+    protected By license_expiry = By.xpath("//body/div[@id='app']/div[@class='oxd-layout orangehrm-upgrade-layout']/div[@class='oxd-layout-container']/div[@class='oxd-layout-context']/div[@class='orangehrm-background-container']/div[@class='orangehrm-card-container']/div[@class='orangehrm-edit-employee']/div[@class='orangehrm-edit-employee-content']/div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']/form[@class='oxd-form']/div[@class='oxd-form-row']/div[@class='oxd-grid-3 orangehrm-full-width-grid']/div[2]/div[1]/div[2]/div[1]/div[1]/input[1]");
+    protected By marital_status = By.xpath("//body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[3]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]");
+    protected By gender_male = By.xpath("//label[normalize-space()='Male']");
+    protected By date_of_birth = By.xpath("//input[@control-id='ControlID-8']");
+    protected By blood_type = By.xpath("//div[@role='option'][normalize-space()='-- Select --']");
+    protected By blood_O_selection = By.xpath("//div[contains(text(),'O+')]");
+
+
+
     public void addNewPIMUser(String Fname,String Mname, String Lname, String id, String Uname,String pass, String conPass)
     {
         Utility.waitForElement(driver, add_btn).click();
@@ -43,6 +57,7 @@ public class AddPIMUserPage
     }
         Faker fake=new Faker();
     //same testcase by using fakers class/library:
+
 
     public void addNewPMIUserUsingFackerClass()
     {
@@ -61,6 +76,19 @@ public class AddPIMUserPage
         Utility.waitForElement(driver, password_con_field).sendKeys("Test@123");
         Utility.waitForElement(driver, save_btn).click();
         Utility.waitForElement(driver, success_msg).isDisplayed();
+        Utility.sleep(3);
+
+        /*Utility.waitForElement(driver, other_id_field).sendKeys(fake.idNumber().invalid());
+        Utility.waitForElement(driver, driving_license).sendKeys(fake.idNumber().valid());
+        Utility.waitForElement(driver, license_expiry).sendKeys("2026-10-15");
+        Utility.waitForElement(driver, marital_status).sendKeys("Married");
+        Utility.waitForElement(driver, gender_male).click();
+        Utility.waitForElement(driver, date_of_birth).sendKeys("2001-10-15");
+        Utility.waitForElement(driver, blood_type).click();
+        Utility.waitForElement(driver, blood_O_selection).click();*/
+
+        Reporter.log("valid ID number is:  "+fake.idNumber().invalid(), true);
+        Reporter.log(driver.getCurrentUrl(), true);
         Reporter.log("Fake fname used is"+fname, true);
         Reporter.log("Fake mname used is"+mname, true);
         Reporter.log("Fake lname used is"+lname, true);
